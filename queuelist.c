ce3576b309d93f9482a7b5bb4d286ef6259a1a76
AmateurECE <edtwardy@mtu.edu>
2017-06-07 21:50:20 -0500

    Updated source to match code standards.

--- queuelist.c
+++ queuelist.c
@@ -1,29 +1,174 @@
-/*
- * NAME: queuelist.c -- the test file for the queue implementation in queuelist.h
- * AUTHOR: Ethan D. Twardy
- * CREATED: 05/09/17
- * LAST EDITED: 05/10/17
- */
+/*******************************************************************************
+ * NAME:	    queuelist.c
+ *
+ * AUTHOR:	    Ethan D. Twardy
+ *
+ * DESCRIPTION:	    Source code for the implementation of a Queue, using a
+ *		    linked list. Follows the function prototypes in queuelist.h.
+ *		    Also contains test code, which is compiled by 'make debug'.
+ *
+ * CREATED:	    05/09/17
+ *
+ * LAST EDITED:	    06/07/17
+ ***/
+
+/*******************************************************************************
+ * INCLUDES
+ ***/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#ifdef CONFIG_DEBUG_QLIST
 #include <time.h>
 #include <assert.h>
+#endif /* CONFIG_DEBUG_QLIST */
 
 #include "queuelist.h"
 
-static inline void error_exit(char * msg)
+/*******************************************************************************
+ * LOCAL PROTOTYPES
+ ***/
+
+#ifdef CONFIG_DEBUG_QLIST
+static inline void error_exit(char *);
+#endif /* CONFIG_DEBUG_QLIST */
+
+/*******************************************************************************
+ * API FUNCTIONS
+ ***/
+
+/*******************************************************************************
+ * FUNCTION:	    queue_init
+ *
+ * DESCRIPTION:	    Initializes a Queue pointer.
+ *
+ * ARGUMENTS:	    queue: (Queue *) -- the queue to be initialized.
+ *		    destroy: (void (*)(void *)) -- pointer to a user defined
+ *			     function meant to free the data held in the queue.
+ *
+ * RETURN:	    void.
+ *
+ * NOTES:	    O(1)
+ ***/
+void queue_init(Queue * queue, void (*destroy)(void *))
 {
-  fprintf(stderr, "%s\n", msg);
-  exit(1);
+  queue->head = NULL;
+  queue->tail = NULL;
+  queue->size = 0;
+  queue->destroy = destroy;
 }
 
+/*******************************************************************************
+ * FUNCTION:	    queue_enqueue
+ *
+ * DESCRIPTION:	    Enqueues the data held in 'data.'
+ *
+ * ARGUMENTS:	    queue: (Queue *) -- the queue to be operated on.
+ *		    data: (void *) -- the data to be enqueued.
+ *
+ * RETURN:	    int -- 0 on success, -1 otherwise.
+ *
+ * NOTES:	    O(1)
+ ***/
+int queue_enqueue(Queue * queue, void * data)
+{
+  QElmt * new;
+
+  if (data == NULL || (new = (QElmt *)malloc(sizeof(QElmt))) == NULL)
+    return -1;
+
+  new->data = data;
+
+  if (queue_isempty(queue)) {
+    /* Handle queueing when the queue is empty. */
+    queue->head = new;
+    queue->tail = new;
+    new->next = NULL;
+
+  } else {
+    /* Handle queueing when it's not. */
+    queue->tail->next = new;
+    new->next = NULL;
+    queue->tail = new;
+
+  }
+
+  queue->size++;
+  return 0;
+}
+
+/*******************************************************************************
+ * FUNCTION:	    queue_dequeue
+ *
+ * DESCRIPTION:	    Dequeues from the queue, and places the data in 'data.'
+ *
+ * ARGUMENTS:	    queue: (Queue *) -- the queue to be operated on.
+ *		    data: (void *) -- once removed, the data is placed here.
+ *
+ * RETURN:	    int -- 0 on success, -1 otherwise.
+ *
+ * NOTES:	    O(1)
+ ***/
+int queue_dequeue(Queue * queue, void ** data)
+{
+  if (data == NULL || queue_isempty(queue))
+    return -1;
+
+  QElmt * old = queue->head;
+  *data = old->data;
+  queue->head = old->next;
+
+  if (queue->head == NULL)
+    queue->tail = NULL;
+  else if (queue->head->next == NULL)
+    queue->tail = queue->head;
+
+  queue->size--;
+  free(old);
+  return 0;
+}
+
+/*******************************************************************************
+ * FUNCTION:	    queue_dest
+ *
+ * DESCRIPTION:	    Removes all data in the queue, and sets all bytes of memory
+ *		    in the Queue structure to be 0.
+ *
+ * ARGUMENTS:	    queue: (Queue *) -- the queue to be operated on.
+ *
+ * RETURN:	    void.
+ *
+ * NOTES:	    O(n)
+ ***/
+void queue_dest(Queue * queue)
+{
+  void * data;
+
+  while (!queue_isempty(queue)) {
+
+    queue_dequeue(queue, (void **)&data);
+
+   if (data != NULL && queue->destroy != NULL)
+      queue->destroy(data);
+
+  }
+
+  memset(queue, 0, sizeof(Queue));
+}
+
+/*******************************************************************************
+ * MAIN
+ ***/
+
+#ifdef CONFIG_DEBUG_QLIST
 int main(int argc, char * arv[])
 {
-  // CASES TO TEST FOR:
-  //    * Normal dequeueing and enqueueing of data. Check order.
-  //    * Check that attempting to dequeue from an empty queue returns -1.
-
+  /* CASES TO TEST FOR:
+      * Normal dequeueing and enqueueing of data. Check order.
+      * Check that attempting to dequeue from an empty queue returns -1.
+   */
   int * pNum = NULL;
   Queue * q;
 
@@ -34,7 +179,7 @@ int main(int argc, char * arv[])
 
   srand((unsigned)time(NULL));
 
-  // Populate the queue with integers.
+  /* Populate the queue with integers. */
   printf("==== Inserting ====\n");
   for (int i = 0; i < 10; i++) {
     pNum = (int *)malloc(sizeof(int));
@@ -46,7 +191,7 @@ int main(int argc, char * arv[])
       printf("int %d @ %p\n", *pNum, pNum);
   }
 
-  // Remove all the integers currently in the queue.
+  /* Remove all the integers currently in the queue. */
   printf("==== Removing =====\n");
   for (int i = 0; i < 10; i++) {
 
@@ -62,3 +207,18 @@ int main(int argc, char * arv[])
   queue_dest(q);
   free(q);
 }
+#endif /* CONFIG_DEBUG_QLIST */
+
+/*******************************************************************************
+ * LOCAL FUNCTIONS
+ ***/
+
+#ifdef CONFIG_DEBUG_QLIST
+static inline void error_exit(char * msg)
+{
+  fprintf(stderr, "%s\n", msg);
+  exit(1);
+}
+#endif /* CONFIG_DEBUG_QLIST */
+
+/******************************************************************************/
--- queuelist.h
+++ queuelist.h
@@ -1,26 +1,28 @@
-/*
- * NAME: queue-list.h -- implementation of a queue using lists, as opposed to arrays.
- * AUTHOR: Ethan D. Twardy
- * CREATED: 05/09/17
- * LAST EDITED: 05/10/17
- */
+/*******************************************************************************
+ * NAME:	    queuelist.h
+ *
+ * AUTHOR:	    Ethan D. Twardy
+ *
+ * DESCRIPTION:	    Header file containing the typedefs and macros for the
+ *		    implementation of a Queue using Lists in queuelist.c.
+ *
+ * CREATED:	    05/09/17
+ *
+ * LAST EDITED:	    06/07/17
+ ***/
 
-/* ************ NOTE ************ */
-/* Due to conflicts between func- */
-/* tion signatures for the public */
-/* interfaces for this structure  */
-/* and the queue using arrays,    */
-/* the two header files cannot    */
-/* be used in the same .c file.   */
-/* ****************************** */
+/* ********************************** NOTE ********************************** */
+/* Due to conflicts between function signatures for the public interfaces for */
+/* this structure and the queue using arrays, the two header files cannot be  */
+/* used in the same .c file. This will be patched in a future update.         */
+/* ************************************************************************** */
 
-#ifndef __ET_QUEUELIST__
-#define __ET_QUEUELIST__
+#ifndef __ET_QUEUELIST_H__
+#define __ET_QUEUELIST_H__
 
-#include <string.h>
-
-/************* TYPE DEFINITIONS *************/
-/********************************************/
+/*******************************************************************************
+ * TYPE DEFINITIONS
+ ***/
 
 typedef struct _QElmt_ {
 
@@ -39,102 +41,23 @@ typedef struct _Queue_ {
 
 } Queue;
 
-/************* MACRO DEFINITIONS ************/
-/********************************************/
+/*******************************************************************************
+ * MACRO DEFINITIONS
+ ***/
 
 #define queue_peek(queue) ((queue)->head)
 #define queue_size(queue) ((queue)->size)
 #define queue_isempty(queue) ((queue)->size == 0 ? 1 : 0)
 
-/************* PUBLIC INTERFACE *************/
-/********************************************/
+/*******************************************************************************
+ * API FUNCTION PROTOTYPES
+ ***/
 
-// FUNCTION: queue_init O(1) -- initializes the queue in queue.
-// PARAMETERS: queue: (Queue *) -- the queue to be initialized.
-//             destroy: void (*)(void *) -- user defined function
-//                      to free all memory allocated with data in a QElmt.
-// RETURN: void.
-void queue_init(Queue * queue, void (*destroy)(void *))
-{
-  queue->head = NULL;
-  queue->tail = NULL;
-  queue->size = 0;
-  queue->destroy = destroy;
-}
+extern void queue_init(Queue *, void (*)(void *));
+extern int queue_enqueue(Queue *, void *);
+extern int queue_dequeue(Queue *, void **);
+extern void queue_dest(Queue *);
 
-// FUNCTION: queue_enqueue O(1) -- enqueues the key data to the end of the list.
-// PARAMETERS: queue: (Queue *) -- the queue to be operated on.
-//             data: (const void *) -- the data to be enqueued.
-// RETURN: int -- 0 on success, -1 on failure.
-int queue_enqueue(Queue * queue, void * data)
-{
-  QElmt * new;
+#endif /* __ET_QUEUELIST_H__ */
 
-  if (data == NULL || (new = (QElmt *)malloc(sizeof(QElmt))) == NULL)
-    return -1;
-
-  new->data = data;
-
-  if (queue_isempty(queue)) {
-    // Handle queueing when the queue is empty.
-    queue->head = new;
-    queue->tail = new;
-    new->next = NULL;
-
-  } else {
-    // Handle queueing when it's not.
-    queue->tail->next = new;
-    new->next = NULL;
-    queue->tail = new;
-
-  }
-
-  queue->size++;
-  return 0;
-}
-
-// FUNCTION: queue_dequeue O(1) -- dequeues the next key and places it in *data.
-// PARAMETERS: queue: (Queue *) -- the queue to be operated on.
-//             data: (void **) -- location to place the key after dequeueing.
-// RETURN: int -- 0 on success, -1 on failure.
-int queue_dequeue(Queue * queue, void ** data)
-{
-  if (data == NULL || queue_isempty(queue))
-    return -1;
-
-  QElmt * old = queue->head;
-  *data = old->data;
-  queue->head = old->next;
-
-  if (queue->head == NULL)
-    queue->tail = NULL;
-  else if (queue->head->next == NULL)
-    queue->tail = queue->head;
-
-  queue->size--;
-  free(old);
-  return 0;
-}
-
-// FUNCTION: queue_dest O(n) -- removes all nodes from the queue and sets
-//           all bytes of the Queue struct to 0. If queue->destroy is set to NULL,
-//           does not free the memory associated with the data.
-// PARAMETERS: queue: (Queue *) -- the queue to be destroyed.
-// RETURN: void.
-void queue_dest(Queue * queue)
-{
-  void * data;
-
-  while (!queue_isempty(queue)) {
-
-    queue_dequeue(queue, (void **)&data);
-
-   if (data != NULL && queue->destroy != NULL)
-      queue->destroy(data);
-
-  }
-
-  memset(queue, 0, sizeof(Queue));
-}
-
-#endif // __ET_QUEUELIST__
+/******************************************************************************/
